31bdce40d1e27fe5ae5e3f4cf39d755bc9d6e638
Timur Karimbaev <timur@karimbaev.com>
2017-10-10 14:48:49 +0300

    change api endpoint protocol

--- humandesign/client-api.js
+++ humandesign/client-api.js
@@ -4,8 +4,7 @@
  ***********************/
 var HumanDesignApi = function( ) {
 
-	var endpointUrl = "http://hd.dating/pl/hd/hd-api/";
-	//var endpointUrl = "http://hddating.getcourse.local/pl/hd/hd-api/";
+	var endpointUrl = "https://hd.dating/pl/hd/hd-api/";
 	var appId = 12913;
 	var appKey = "6bf4db0db5053e2b09ab99d662106ea2";
 
